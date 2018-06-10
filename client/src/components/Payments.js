affb789af46e33f63fa85ef37e6c6b89d07501fb
Srikanth Dorairaj <srikanth@Srikanths-MBP.connect>
2018-06-10 19:30:02 +0100

    Added client side App

---
+++ client/src/components/Payments.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Payments extends Component {
+    render() {
+        return (
+            <StripeCheckout
+                name="Emaily"
+                description="£5 for 5 email credits"
+                token={token => this.props.handleToken(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+                amount={500}
+                currency="GBP"
+            >
+                <button className="btn">Add Credits</button>
+            </StripeCheckout>
+        )
+    }
+}
+
+export default connect(null, actions)(Payments);
